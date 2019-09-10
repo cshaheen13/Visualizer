2a311b9c6514d6cf175d4536ad880780166a4970
cshaheen13 <cshaheen13@gmail.com>
2019-09-09 22:33:21 -0700

    03_MainMenu

--- Source/Visualizer/DynamicsActor.cpp
+++ Source/Visualizer/DynamicsActor.cpp
@@ -2,6 +2,16 @@
 
 
 #include "DynamicsActor.h"
+#include "Engine/World.h"
+#include "Runtime/Core/Public/Math/UnrealMathUtility.h"
+#include "Runtime/Engine/Classes/Components/TextRenderComponent.h"
+#include "Runtime/Engine/Classes/Engine/TextRenderActor.h"
+#include "Components/TextRenderComponent.h"
+#include "Runtime/Engine/Classes/Engine/RectLight.h"
+#include "Runtime/Engine/Classes/Components/RectLightComponent.h"
+#include "Runtime/Engine/Classes/Components/SceneComponent.h"
+#include "Runtime/Engine/Classes/Engine/EngineTypes.h"
+
 
 // Sets default values
 ADynamicsActor::ADynamicsActor()
@@ -15,23 +25,72 @@ void ADynamicsActor::BeginPlay()
 {
 	Super::BeginPlay();
 	SetActorTickEnabled(false);
+	SetAccelX(KineticFriction, Gravity);
 
+	float RandDistance = FMath::RandRange(300, 1500);
+	ADynamicsActor::SetWinDistance(RandDistance, 0.0f, 0.0f);
+	UE_LOG(LogClass, Warning, TEXT("Rand = %f"), RandDistance);
+	FString WinDistanceString = "X = " + (FString::SanitizeFloat(RandDistance/100)) + " meters";
+	WinLightLocation = FVector(RandDistance, 865, 40);
+	WinLightRotation = FRotator(-90, 270, -180);
+
+	WinDistanceText = GetWorld()->SpawnActor<ATextRenderActor>(ATextRenderActor::StaticClass(), FVector(RandDistance, 930.0f, -10.0f), FRotator(0.0f, 90.0f, 0.0f));
+	WinDistanceText->GetTextRender()->SetText(FString(WinDistanceString));
+	WinDistanceText->GetTextRender()->SetTextRenderColor(FColor::Red);
+	WinDistanceText->SetActorScale3D(FVector(0.0f, 1.5f, 1.5f));
+
+	WinLight = GetWorld()->SpawnActor<ARectLight>(WinLightLocation, WinLightRotation);
+	WinLight->SetLightColor(FLinearColor::Blue);
+	//***Cast Component onto Actor***
+	URectLightComponent * GetWinLight = Cast<URectLightComponent>(WinLight->GetComponentByClass(URectLightComponent::StaticClass()));
+	GetWinLight->SetMobility(EComponentMobility::Movable);
+	GetWinLight->SetSourceWidth(115);
+	GetWinLight->SetSourceHeight(115);
+}
+
+void ADynamicsActor::SetWinDistance(float x, float y, float z)
+{
+	WinDistance = FVector(x, y, z);
+}
+
+FVector ADynamicsActor::GetWinDistance()
+{
+	return WinDistance;
+}
+
+float ADynamicsActor::GetAccelX()
+{
+	return AccelX;
+}
+
+void ADynamicsActor::SetAccelX(float KineticFriction, float Gravity)
+{
+	AccelX = KineticFriction * Gravity;
 }
 
 void ADynamicsActor::MoveActor(float DeltaTime)
 {
 	FVector NewLocation = GetActorLocation();
-	float accelX = KineticFriction * Gravity;
-	//float finalTime = -(InitialVelocity / accelX);
-	float deltaX = 100 * ((InitialVelocity * RunningTime) + ((accelX)*(RunningTime)*(RunningTime)*.5));
-	UE_LOG(LogClass, Warning, TEXT("t = %f"), RunningTime);
-	UE_LOG(LogClass, Warning, TEXT("X = %f"), deltaX);
+	FVector OldLocation = GetActorLocation();
+	float deltaX = 100 * ((InitialVelocity * RunningTime) + ((ADynamicsActor::GetAccelX())*(RunningTime)*(RunningTime)*.5));
+	//UE_LOG(LogClass, Warning, TEXT("t = %f"), RunningTime);
+	//UE_LOG(LogClass, Warning, TEXT("X = %f"), deltaX);
 
-	if ((deltaX - NewLocation.X) >= 0)
+	if (((deltaX - NewLocation.X) >= 0))
 	{
 		NewLocation.X = deltaX;
 		RunningTime += DeltaTime;
 		SetActorLocation(NewLocation);
+		UE_LOG(LogClass, Warning, TEXT("%f"), NewLocation.X - OldLocation.X);
+	}
+	if ((RunningTime != 0) && ((NewLocation.X - OldLocation.X) <= 0.02))
+	{
+		FVector WinLoseDist = GetWinDistance();
+		if (((WinLoseDist.X - NewLocation.X) < 0.1f) && ((WinLoseDist.X - NewLocation.X) > -0.1f))
+		{
+			YouWinBool = true;
+			UE_LOG(LogClass, Warning, TEXT("You Win! %f"), WinLoseDist.X - NewLocation.X);
+		}
 	}
 }
 
--- Source/Visualizer/DynamicsActor.h
+++ Source/Visualizer/DynamicsActor.h
@@ -3,8 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Runtime/Engine/Classes/GameFramework/Actor.h"
 #include "DynamicsActor.generated.h"
+
+
 
 UCLASS()
 class VISUALIZER_API ADynamicsActor : public AActor
@@ -15,23 +17,31 @@ public:
 	// Sets default values for this actor's properties
 	ADynamicsActor();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	void MoveActor(float RunningTime);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class AActor* ActorThatWins;
 
 	UPROPERTY(BlueprintReadWrite)
 	bool testPush = false;
 
-	float RunningTime;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float InitialVelocity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FVector WinDistance;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:	
+
+	class ARectLight* WinLight;
+
+	class ATextRenderActor* WinDistanceText;
+		
+	float RunningTime;
+
+	bool YouWinBool = false;
 
 	UPROPERTY(EditAnywhere)
 	float KineticFriction;
@@ -39,6 +49,25 @@ public:
 	UPROPERTY(EditAnywhere)
 	float Gravity;
 
-	float accelX;
+	float AccelX;
+
+	float GetAccelX();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void SetAccelX(float KineticFriction, float Gravity);
+
+	FVector WinLightLocation;
+
+	FRotator WinLightRotation;
+
+	FVector GetWinDistance();
+
+	void SetWinDistance(float x, float y, float z);
+
+	void MoveActor(float RunningTime);
+
+	void CheckWinLose();
 
 };
